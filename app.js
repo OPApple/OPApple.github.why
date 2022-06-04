b3ace78a661f12eaf278e5ea25b980e165042167
OPApple <80098382+OPApple@users.noreply.github.com>
2022-06-04 16:08:15 +0300

    Made the website

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const defList = ["Päivä (ひ、にち)", "yksi (いち、ひと)", "Valtio (こく、くに)", "Henkilö (ひと、じん)", "Vuosi (ねん)"]
 const kanjiList = ["日", `一`, `国`, `人`, `年`]
 
-let max = defList.length - 1
+var max = defList.length - 1
 var rand = Math.round((Math.random() * max))
 
 function pickQuestion (){
-
+    rand = Math.round((Math.random() * max))
     document.getElementById('question').innerHTML = defList[rand]
 
+}
+
+
+function startUp (){
+    document.getElementById('question').innerHTML = defList[rand]
 
 }
-pickQuestion()
 
 function showAnswer(){
     document.getElementById('answer').innerHTML = kanjiList[rand]
-    console.log(kanjiList[rand])
 }
 
 function next(){
-    
+    pickQuestion()
+    document.getElementById('answer').innerHTML = ""
 }
+
+startUp()
