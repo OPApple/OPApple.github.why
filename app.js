2d54cb6f3d81d1c4adc4d8bdaa5372fa992d2a2c
OPApple <80098382+OPApple@users.noreply.github.com>
2022-06-04 16:26:16 +0300

    added more kanjis and did some style things

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const defList = ["Päivä (ひ、にち)", "yksi (いち、ひと)", "Valtio (こく、くに)", "Henkilö (ひと、じん)", "Vuosi (ねん)"]
-const kanjiList = ["日", `一`, `国`, `人`, `年`]
+const defList = [
+    
+    "Päivä (ひ、にち)", "yksi (いち、ひと)", "Valtio (こく、くに)", "Henkilö (ひと、じん)", "Vuosi (ねん)", "Suuri (だい、おお)", "Kymmenen (じゅう)",
+    "Kaksi (に、ふた)", "Kirja (ほん)", "Keskellä/sisällä (ちゅう、なか)", "Pitkä (なが。い)", "Poistua/lähteä (で)", "Kolme (さん、み)", "Aika (じ、とき)",
+    "Mennä (い)", "Nähdä (み)", "Kuu(kausi) (つき)"
+
+
+]
+const kanjiList = ["日", `一`, `国`, `人`, `年`, `大`, `十`, `二`, `本`, `中`, `長`, `出`, `三`, `時`, `行`, `見`, `月`]
 
 var max = defList.length - 1
 var rand = Math.round((Math.random() * max))
