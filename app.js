62ef2f14ecbced05c9e84cb85041b7c4148a7a46
OPApple <80098382+OPApple@users.noreply.github.com>
2022-06-10 16:13:55 +0300

    Made the website???

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-const defList = [
-    
-    "Päivä (ひ、にち)", "yksi (いち、ひと)", "Valtio (こく、くに)", "Henkilö (ひと、じん)", "Vuosi (ねん)", "Suuri (だい、おお)", "Kymmenen (じゅう)",
-    "Kaksi (に、ふた)", "Kirja (ほん)", "Keskellä/sisällä (ちゅう、なか)", "Pitkä (なが。い)", "Poistua/lähteä (で)", "Kolme (さん、み)", "Aika (じ、とき)",
-    "Mennä (い)", "Nähdä (み)", "Kuu(kausi) (つき)"
+const days = []
+const months = []
 
-
-]
-const kanjiList = ["日", `一`, `国`, `人`, `年`, `大`, `十`, `二`, `本`, `中`, `長`, `出`, `三`, `時`, `行`, `見`, `月`]
-
-var max = defList.length - 1
-var rand = Math.round((Math.random() * max))
-
-function pickQuestion (){
-    rand = Math.round((Math.random() * max))
-    document.getElementById('question').innerHTML = defList[rand]
-
+for (let i = 1; i <= 365; i++){
+    days.push(i)
 }
 
-
-function startUp (){
-    document.getElementById('question').innerHTML = defList[rand]
-
+for (let i = 1; i <= 12; i++){
+    months.push(i)
 }
 
-function showAnswer(){
-    document.getElementById('answer').innerHTML = kanjiList[rand]
-}
+function calculateDeath (){
+    let ranDays = Math.round(Math.random() * (30 - 1) + 1)
+    let ranMonths = Math.round(Math.random() * (12 - 1) + 1)
+    let ranYear = Math.round(Math.random() * (2100 - 2022) + 2022)
 
-function next(){
-    pickQuestion()
-    document.getElementById('answer').innerHTML = ""
-}
+    let dd = ranDays + "." + ranMonths + "." + ranYear
 
-startUp()
+    console.log(dd)
+
+    document.getElementById('death').innerHTML = dd 
+}
